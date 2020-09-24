f8c783b58652e3792014c7ec51b282d7df0a48b2
chiakicage <841851568@qq.com>
2020-09-24 14:18:27 +0800

    test

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -13,19 +13,37 @@ addbutton.onclick = function () {
         let d = document.createElement('div');
         d.setAttribute('class', 'task-item');
         let pt = document.createElement('input');
-        pt.setAttribute('id', 'task' + String(taskCount));
+        // pt.setAttribute('id', 'task' + String(taskCount));
         pt.setAttribute('type', 'checkbox');
+        pt.setAttribute('style', 'zoom:180%');
         pt.setAttribute('class', 'check-box');
-        let lb = document.createElement('label');
-        lb.setAttribute('for', 'task' + String(taskCount));
-        lb.setAttribute('class', 'taskmsg');
-        lb.innerText = addtask.value;
+        let lb = document.createElement('input');
+        // lb.setAttribute('for', 'task' + String(taskCount));
+        lb.setAttribute('type', 'text');
+        // lb.setAttribute('disabled', '');
+        lb.setAttribute('class', 'taskmsg-on');
+        lb.setAttribute('value', addtask.value);
+        pt.onclick = function() {
+            if (lb.getAttribute('class') === 'taskmsg-on') {
+                lb.setAttribute('class', 'taskmsg-off');
+            } else {
+                lb.setAttribute('class', 'taskmsg-on');
+            }
+        }
+        // lb.ondbclick = function() {
+            // lb.removeAttribute('disabled');
+            // lb.focus();
+        // }
         addtask.value = "";
         d.appendChild(pt);
         d.appendChild(lb);
         tasklist.appendChild(d);
     }
 }
+addtask.value = 'test';
+addbutton.onclick();
+addtask.value = 'test';
+addbutton.onclick();
 // let form = document.getElementById('mainform');
 // form.onsubmit = addbutton.onclick;
 
