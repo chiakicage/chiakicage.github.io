2b0ed9f984d24546fa39999ca7cec6ae591f25c2
chiakicage <841851568@qq.com>
2020-09-25 01:18:27 +0800

    tests

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -4,84 +4,144 @@ let addtask = document.getElementById('taskinfo');
 let addbutton = document.getElementById('task-add');
 let tasklist1 = document.getElementById('todoTask1');
 let tasklist2 = document.getElementById('todoTask2');
+let taskCount = 0;
 // console.log(searchbox, searchbutton, taskmsg, addbutton);
 
 function enterAdd(e) {
-    if (e.keyCode == 13) 
+    if (e.keyCode == 13)
         addbutton.click();
 }
 
-function Update() {
-    
-}
-// function 
+let upd = new XMLHttpRequest();
+let rm = new XMLHttpRequest();
+let add = new XMLHttpRequest();
+let url = 'http://localhost:8000/'
 
+add.onreadystatechange = function () {
+    console.log('123');
+}
+
+function updateTask(id, value, group) {
+    upd.open('GET', url + 'update?id=' + String(id) + '&value=' + String(value) + '&group=' + String(group));
+    upd.send();
+}
+
+function removeTask(id) {
+    rm.open('GET', url + 'remove?id=' + String(id));
+    rm.send();
+}
+
+function addTask(id, value) {
+    add.open('GET', url + 'add?id=' + String(id) + '&value=' + String(value));
+    add.send();
+}
+
+function addTaskn(value, group, id) {
+    let d = document.createElement('div');
+    d.setAttribute('class', 'task-item');
+    d.setAttribute('id', String(id));
+    let pt = document.createElement('input');
+    pt.setAttribute('id', 'test' + String(taskCount));
+    pt.setAttribute('type', 'checkbox');
+    pt.setAttribute('style', 'zoom:180%');
+    pt.setAttribute('class', 'check-box');
+    let lb = document.createElement('input');
+    lb.setAttribute('value', value);
+    lb.setAttribute('edited', value);
+    lb.setAttribute('class', 'taskmsg-on');
+    lb.setAttribute('type', 'text');
+    lb.setAttribute('editing', 'false');
+    lb.onfocus = function () {
+        if (lb.getAttribute('editing') === 'false')
+            lb.blur();
+    }
+    lb.onblur = function () {
+        lb.setAttribute('editing', 'false');
+        if (lb.getAttribute('edited') !== lb.getAttribute('value')) {
+            updateTask(d.getAttribute('id'), lb.getAttribute('value'), lb.getAttribute('class') === 'taskmsg-on' ? 1 : 2);
+            lb.getAttribute('edited') = lb.getAttribute('value');
+        }
+    }
+    pt.onclick = function () {
+        if (lb.getAttribute('class') === 'taskmsg-on') {
+            lb.setAttribute('class', 'taskmsg-off');
+            updateTask(d.getAttribute('id'), lb.getAttribute('value'), 2);
+            tasklist2.appendChild(tasklist1.removeChild(d))
+        } else {
+            lb.setAttribute('class', 'taskmsg-on');
+            updateTask(d.getAttribute('id'), lb.getAttribute('value'), 1);
+            tasklist1.appendChild(tasklist2.removeChild(d))
+        }
+    }
+    let de = document.createElement('img');
+    de.setAttribute('src', 'images/trash-can.svg');
+    de.setAttribute('class', 'del');
+    de.onclick = function () {
+        d.parentElement.removeChild(d);
+        removeTask(d.getAttribute('id'));
+    }
+    let ed = document.createElement('img');
+    ed.setAttribute('src', 'images/pencil.svg');
+    ed.setAttribute('class', 'edit');
+    ed.onclick = function () {
+        lb.setAttribute('editing', 'true');
+        lb.focus();
+    }
+    d.appendChild(pt);
+    d.appendChild(lb);
+    d.appendChild(ed);
+    d.appendChild(de);
+    tasklist1.appendChild(d);
+    if (group === 2) {
+        pt.click();
+    }
+}
+
+
+function initialize() {
+    let init = new XMLHttpRequest();
+    init.onreadystatechange = function () {
+        if (init.readyState === 4) {
+            if (init.status === 200) {
+                arr = JSON.parse(init.responseText) || [];
+                // console.log(arr);
+                arr.forEach(element => {
+                    return addTaskn(element.value, element.group, element.id);
+                });
+                console.log(arr);
+            } else {
+                alert('Failed');
+            }
+        }
+    }
+    init.open('GET', url + 'init');
+    init.send();
+    let init2 = new XMLHttpRequest();
+    init2.onreadystatechange = function () {
+        if (init2.readyState === 4) {
+            if (init2.status === 200) {
+                taskCount = Number(init2.responseText);
+                console.log(taskCount);
+            } else {
+                alert('Failed');
+            }
+        }
+    }
+    init2.open('GET', url + 'init2');
+    init2.send();
+}
 addbutton.onclick = function () {
     if (addtask.value === "") {
         return;
     } else {
-        let d = document.createElement('div');
-        d.setAttribute('class', 'task-item');
-        let pt = document.createElement('input');
-        // pt.setAttribute('id', 'task' + String(taskCount));
-        pt.setAttribute('type', 'checkbox');
-        pt.setAttribute('style', 'zoom:180%');
-        pt.setAttribute('class', 'check-box');
-        let lb = document.createElement('input');
-        lb.setAttribute('value', addtask.value);
-        lb.setAttribute('class', 'taskmsg-on');
-        lb.setAttribute('type', 'text');
-        lb.setAttribute('edited', addtask.value);
-        lb.setAttribute('editing', 'false');
-        lb.onfocus = function() {
-            if (lb.getAttribute('editing') === 'false')
-                lb.blur();
-        }
-        lb.onblur = function() {
-            lb.setAttribute('editing', 'false');
-            if (addtask.value !== lb.value) {
-                Update();
-            }
-        }
-        pt.onclick = function() {
-            if (lb.getAttribute('class') === 'taskmsg-on') {
-                lb.setAttribute('class', 'taskmsg-off');
-                tasklist2.appendChild(tasklist1.removeChild(d))
-            } else {
-                lb.setAttribute('class', 'taskmsg-on');
-                tasklist1.appendChild(tasklist2.removeChild(d))
-            }
-        }
-        let de = document.createElement('img');
-        de.setAttribute('src', 'images/trash-can.svg');
-        de.setAttribute('class', 'del');
-        de.onclick = function() {
-            d.parentElement.removeChild(d);
-        }
-        let ed = document.createElement('img');
-        ed.setAttribute('src', 'images/pencil.svg');
-        ed.setAttribute('class', 'edit');
-        ed.onclick = function() {
-            lb.setAttribute('editing', 'true');
-            lb.focus();
-        }
+        addTaskn(addtask.value, 1, ++taskCount);
+        addTask(taskCount, addtask.value);
         addtask.value = "";
-        d.appendChild(pt);
-        d.appendChild(lb);
-        d.appendChild(ed);
-        d.appendChild(de);
-        tasklist1.appendChild(d);
     }
 }
-addtask.value = 'test';
-addbutton.onclick();
-addtask.value = 'test';
-addbutton.onclick();
 
+initialize();
 
-// let form = document.getElementById('mainform');
-// form.onsubmit = addbutton.onclick;
-
-
-
+// addtask.value = "?";
+// addbutton.click();
 
--- test-site/server.js
+++ test-site/server.js
@@ -1,17 +1,53 @@
+const { timeStamp } = require('console');
+const { resolveNaptr } = require('dns');
 var http = require('http');
-a = {
-    class : 1,
-    opt : 0
-};
-str = JSON.stringify(a);
-console.log(str);
-let server = http.createServer(function (request, response) {
-    console.log(request);
-    response.writeHead(200, {
-        "Access-Control-Allow-Origin": "*"
-    });
+var url = require('url');
+let data = [{
+    value: "test",
+    group: 1,
+    removed: true,
+    id: 0
+}];
 
-    response.end(str);
+let server = http.createServer(function (request, response) {
+    console.log(url.parse(request.url));
+    let t = url.parse(request.url);
+    response.writeHead(200, {
+        'Access-Control-Allow-Origin': '*',
+    });
+    if (t.pathname === '/add') {
+        let s = t.query.split('&');
+        console.log(s);
+        data.push({
+            value: (s[1].split('='))[1],
+            group: 1,
+            removed: false,
+            id: Number((s[0].split('='))[1])
+        });
+        response.end('Success');
+    } else if (t.pathname === '/update') {
+        let s = t.query.split('&');
+        let p = Number((s[0].split('='))[1]);
+        data[p].value = (s[1].split('='))[1];
+        data[p].group = Number((s[2].split('='))[1]);
+        response.end('Success');
+    } else if (t.pathname === '/remove') {
+        let s = t.query.split('&');
+        let p = Number((s[0].split('='))[1]);
+        data[p].removed = true;
+        response.end('Success');
+    } else if (t.pathname === '/init') {
+        console.log(2);
+        let tmp = [];
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].removed === false)
+                tmp.push(data[i]);
+        }
+        response.end(JSON.stringify(tmp));
+    } else if (t.pathname === '/init2') {
+        response.end(String(data.length - 1));
+    }
+    console.log(data);
 });
 
 server.listen(8000);
